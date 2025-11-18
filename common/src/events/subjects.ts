a2fa33604892df845342db224b917ca277b5b373
Rayloveyou <datnghiem17062003@gmail.com>
2025-11-18 17:34:44 +0700

    Updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -1,11 +1,11 @@
 export enum Subjects {
-    ProductCreated = 'product:created',
-    ProductUpdated = 'product:updated',
+    ProductCreated = 'product.created',
+    ProductUpdated = 'product.updated',
 
-    OrderCreated = 'order:created',
-    OrderCancelled = 'order:cancelled',
+    OrderCreated = 'order.created',
+    OrderCancelled = 'order.cancelled',
     
-    PaymentCreated = 'payment:created',
+    PaymentCreated = 'payment.created',
 
-    CartCheckout = 'cart:checkout'
+    CartCheckout = 'cart.checkout'
 }
